5c4bf41d96160dc88c072c28144145e5ac0a61d7
GavinCui <garrettcui@outlook.com>
2025-11-12 21:27:21 +0800

    Simple AI Application using ollama Service

---
+++ models/chat/ollama/src/test/java/com/example/spring/ai/ollama/OllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.ai.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
